2f17c8bdc9b98384e4dfefc848e6f07fcb5499db
daos00-pen <daos00.pen@gmail.com>
2025-05-04 01:29:33 +0200

    origin

--- waste.py
+++ waste.py
@@ -5,6 +5,7 @@ from selenium.webdriver.chrome.options import Options
 
 options = Options()
 options.add_argument("--headless=new")
+options.add_argument('--disable-gpu')
 
 driver = webdriver.Chrome(options=options)
 
