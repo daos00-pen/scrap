9c2b330b2b50cf4cd67102ca92ea014191487d20
daos00-pen <daos00.pen@gmail.com>
2025-05-04 01:38:23 +0200

    scre

--- waste.py
+++ waste.py
@@ -2,12 +2,14 @@ import streamlit as st
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 
 options = Options()
 options.add_argument("--headless=new")
 options.add_argument('--disable-gpu')
 
-driver = webdriver.Chrome(options=options)
+service = Service()
+driver = webdriver.Chrome(service=service, options=options)
 
 driver.get('https://www.gov.uk/')
 
