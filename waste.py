27ff969f213802afc4a6c0d45352b4b65b5260fc
daos00-pen <daos00.pen@gmail.com>
2025-05-04 01:41:19 +0200

    screenshot-copy

--- waste.py
+++ waste.py
@@ -1,16 +1,23 @@
 import streamlit as st
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 
-options = Options()
-options.add_argument("--headless=new")
+options = webdriver.ChromeOptions()
 options.add_argument('--disable-gpu')
+options.add_argument('--headless')
+options.add_argument(f"--window-size={width}x{height}")
 
 service = Service()
 driver = webdriver.Chrome(service=service, options=options)
 
-driver.get('https://www.gov.uk/')
+driver = get_driver(https://www.gov.uk/)
+        
+time.sleep(3)
+# Explicitly wait for an essential element to ensure content is loaded
+WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, 'body')))
 
 st.write(driver.page_source)
